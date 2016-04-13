1af0ecb838116cc2f2bfea9e932efbae3d632c74
CBorum <christopherborum@gmail.com>
2016-04-13 21:01:49 +0200

    create user feature

--- web/app/view6/view6.js
+++ web/app/view6/view6.js
@@ -2,8 +2,36 @@
 
 angular.module('myApp.view6', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view6', {
-    templateUrl: 'app/view6/view6.html'
-  });
-}]);
+        .config(['$routeProvider', function ($routeProvider) {
+                $routeProvider.when('/view6', {
+                    templateUrl: 'app/view6/view6.html'
+                });
+            }])
+        .controller('formController', ['$scope', '$http', function ($scope, $http) {
+                $scope.create = function (user) {
+                    if (user.userName.length < 1) {
+                        $('#view6 .alert').remove();
+                        $('#view6').prepend('<div class="alert alert-danger id="message-box">Enter a username</div>');
+                        return;
+                    } else if (user.password.length < 4) {
+                        $('#view6 .alert').remove();
+                        $('#view6').prepend('<div class="alert alert-danger id="message-box">Password length needs to be longer than 4</div>');
+                        return;
+                    }
+                    var data = {
+                        userName: user.userName,
+                        password: user.password
+                    };
+                    $http.post('/SemesterSeed/api/createuser', data).success(function (data) {
+                        var dataBool = data === "ok";
+                        $('#view6 .alert').remove();
+                        $('#view6').prepend('<div class="alert ' 
+                                + (dataBool ? 'alert-success' : 'alert-danger') 
+                                + '" id="message-box">' 
+                                + (dataBool ? 'Account created' : 'Username taken') + '</div>');
+                        if (dataBool) {
+                            $('#createUserForm').remove();
+                        }
+                    });
+                };
+            }]);
