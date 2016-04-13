abe41a182b1e96474ee928346276ca4dce13f96f
Kristian <kristian.o.g.nielsen@gmail.com>
2016-04-13 22:40:36 +0200

    rate backend næsten færdig (problemer med Rest)

--- src/java/rest/ApplicationConfig.java
+++ src/java/rest/ApplicationConfig.java
@@ -25,10 +25,11 @@ public class ApplicationConfig extends Application {
     resources.add(httpErrors.NotFoundExceptionMapper.class);
     resources.add(rest.Admin.class);
     resources.add(rest.CreateUser.class);
+    resources.add(rest.Rate.class);
     resources.add(rest.User.class);
     resources.add(security.JWTAuthenticationFilter.class);
     resources.add(security.Login.class);
-    resources.add(security.NotAuthorizedExceptionMapper.class);
+        resources.add(security.NotAuthorizedExceptionMapper.class);
         resources.add(security.RolesAllowedFilter.class);
   }
   
