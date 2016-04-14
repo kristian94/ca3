9c03472c0a7585f8e799a2cb5c73754b32cae793
Kristian <kristian.o.g.nielsen@gmail.com>
2016-04-14 11:30:13 +0200

    xmlreader henter 5 dages rates

--- src/java/executorService/XmlReader.java
+++ src/java/executorService/XmlReader.java
@@ -14,13 +14,21 @@ import org.xml.sax.*;
 import org.xml.sax.helpers.*;
 import java.net.URL;
 import java.sql.Date;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class XmlReader extends DefaultHandler {
-    
-    Date date;
-    CurrencyFacade cf = new CurrencyFacade();
-    RateFacade drf = new RateFacade();
-    
+
+    private Date date = null;
+    private CurrencyFacade cf = new CurrencyFacade();
+    private RateFacade drf = new RateFacade();
+    private NumberFormat format = NumberFormat.getInstance(Locale.FRANCE);
+
     @Override
     public void startDocument() throws SAXException {
     }
@@ -31,56 +39,111 @@ public class XmlReader extends DefaultHandler {
 
     @Override
     public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+
+//        if(localName.equals("dailyrates")){
+//            date = Date.valueOf(attributes.getValue(0));
+//            return;
+//        }
+//        if(localName.equals("currency")) {
+//       
+//            Currency currency = new Currency();
+//            Rate rate = new Rate();
+//
+//            currency.setCode(attributes.getValue(0));
+//            currency.setDesc(attributes.getValue(1));
+//            
+//            System.out.println(currency.getCode());
+//            
+//            
+//            
+//
+//            rate.setCurrency(currency);
+//            rate.setDate(date);
+//            try{
+//                rate.setRate(Double.parseDouble(attributes.getValue(2)));
+//            }catch(NumberFormatException e){
+//                rate.setRate(-1);
+//            }
+//            
+//            
+//            currency.addRate(rate);
+//            
+//            cf.addCurrency(currency);
+        List<Rate> rates = new ArrayList();
+        List<Currency> currencies = new ArrayList();
+
         
+
         
-        
-        
-        if(localName.equals("dailyrates")){
-            date = Date.valueOf(attributes.getValue(0));
-            return;
+
+        if (attributes.getValue("time") != null) {
+            date = Date.valueOf(attributes.getValue("time"));
+        } else if (attributes.getValue("currency") != null) {
+            Rate r = new Rate();
+            Currency c = new Currency();
+            r.setCurrency(c);
+            r.setDate(date);
+            
+//            r.setRate(Double.parseDouble(attributes.getValue("rate")));
+            try{
+                r.setRate(format.parse(attributes.getValue("rate")).doubleValue());
+            } catch (ParseException ex) {
+                r.setRate(-1);
+            }
+                
+            c.addRate(r);
+            c.setCode(attributes.getValue("currency"));
+            c.setDesc(attributes.getValue("name"));
+            
+            
+            cf.addCurrency(c);
+            
         }
-        if(localName.equals("currency")) {
-       
+
+//            switch(attributes.getQName(i)){
+//                case("time"):
+//                    
+//                    break;
+//                case("currency"):
+//                    
+//                    
+//            }
+        if (localName.equals("currency")) {
+
             Currency currency = new Currency();
             Rate rate = new Rate();
 
             currency.setCode(attributes.getValue(0));
             currency.setDesc(attributes.getValue(1));
-            
+
             System.out.println(currency.getCode());
-            
-            
-            
 
             rate.setCurrency(currency);
             rate.setDate(date);
-            try{
+            try {
                 rate.setRate(Double.parseDouble(attributes.getValue(2)));
-            }catch(NumberFormatException e){
+            } catch (NumberFormatException e) {
                 rate.setRate(-1);
             }
-            
-            
+
             currency.addRate(rate);
-            
+
             cf.addCurrency(currency);
-            
- 
+
         }
     }
-
 
     public void getDailyRate() {
         try {
             XMLReader xr = XMLReaderFactory.createXMLReader();
-            
+            System.out.println("hey");
             xr.setContentHandler(new XmlReader());
-            URL url = new URL("http://www.nationalbanken.dk/_vti_bin/DN/DataService.svc/CurrencyRatesXML?lang=en");
+            URL url = new URL("http://www.nationalbanken.dk/_vti_bin/DN/DataService.svc/CurrencyRatesHistoryXML?lang=da");
             xr.parse(new InputSource(url.openStream()));
         } catch (SAXException | IOException e) {
             e.printStackTrace();
         }
-        
+
     }
 
 }
